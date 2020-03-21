06f42a16eff6e236ac56d846ee63c9b727c4eacf
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-03-21 13:42:57 -0400

    chore: putstrln errthing

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,7 +10,7 @@ main = launch
 
 launch :: IO ()
 launch = do
-  putStr "Pick a hidden number (press Enter to continue)"
+  putStrLn "Pick a hidden number (press Enter to continue)"
   l <- getLine
   result <- nextGuess []
   if isNothing result
@@ -28,13 +28,13 @@ nextGuess prevGuessResults = do
     else do
       let guess = head possible
       putStrLn ("My guess is: " ++ showGuess guess)
-      putStr "How many dead? "
+      putStrLn "How many dead? "
       l <- getLine
       let deadN = read l :: Int
       if deadN == 4
         then return (Just (GuessResult guess 4 0))
         else do
-          putStr "How many injured? "
+          putStrLn "How many injured? "
           l <- getLine
           let injuredN = read l :: Int
           nextGuess (GuessResult guess deadN injuredN : prevGuessResults)
