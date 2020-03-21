793e1c230a887e73daa552601655ffd5772c185e
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-03-21 11:54:52 -0400

    wip: guess module

---
+++ src/Guess.hs
@@ -0,0 +1,67 @@
+module Guess
+  ( Guess
+  , GuessResult
+  , limitPossible
+  ) where
+
+import           Data.List
+
+-- Given a list of results get all the remaining possible guesses
+limitPossible :: [GuessResult] -> [Guess]
+limitPossible constraints =
+  [guess | guess <- allPossible, all (isPossible guess) constraints]
+
+-- All possible guesses in the game
+allPossible :: [Guess]
+allPossible =
+  [ (a, b, c, d)
+  | a <- validRange
+  , b <- validRange
+  , c <- validRange
+  , d <- validRange
+  , isValid (a, b, c, d)
+  ]
+
+-- Given a result check if a guess is still possibly correct
+isPossible :: Guess -> GuessResult -> Bool
+isPossible g r = countDead g' g == dead r && countInjured g' g == injured r
+  where
+    g' = guess r
+
+-- Checks if a guess is valid i.e. has no repeated numbers
+isValid :: Guess -> Bool
+isValid (a, b, c, d) = length (nub xs) == length xs
+  where
+    xs = [a, b, c, d]
+
+-- Check how many numbers in the guess match in the same possition
+countDead :: Guess -> Guess -> Int
+countDead (a, b, c, d) (w, x, y, z) =
+  sum $ map fromEnum [a == w, b == x, c == y, d == z]
+
+-- Check how many numbers in the guess match but not in the same position
+countInjured :: Guess -> Guess -> Int
+countInjured (a, b, c, d) (w, x, y, z) =
+  sum $
+  map
+    fromEnum
+    [ a `elem` [x, y, z]
+    , b `elem` [w, y, z]
+    , c `elem` [w, x, z]
+    , d `elem` [w, x, y]
+    ]
+
+-- Range of valid numbers allowed in a guess
+validRange = [1 .. 9]
+
+-- guess result
+data GuessResult =
+  GuessResult
+    { guess   :: Guess
+    , dead    :: Int
+    , injured :: Int
+    }
+  deriving (Show)
+
+-- guess type
+type Guess = (Int, Int, Int, Int)
